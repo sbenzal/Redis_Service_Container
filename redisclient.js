90af9ce152b961fa7fdb1722b4a37e4398fc936b
sbenzal <sebabenzal97@gmail.com>
2023-10-31 16:40:56 -0300

    no se

--- redisclient.js
+++ redisclient.js
@@ -1,11 +1,11 @@
-const redis = require("redis");
+const redis = require('redis');
 
 // Creates a new Redis client
 // In the workflow we are going ot set If REDIS_HOST and REDIS_PORT
 
 const redisClient = redis.createClient({
     host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
+    port: process.env.REDIS_PORT,
 });
 
 redisClient.on("error", function(err) {
@@ -33,7 +33,7 @@ redisClient.hGet('spanish', 'red', (err, value) => {
 redisClient.hKeys("german", function (err, germankeys) {
     console.log(germankeys.length + " germanWords:");
     germankeys.forEach(function (germankey, i) {
-        redisClient.hGet('spanish', germankey, 
+        redisClient.hGet('german', germankey, 
         (err, value) => {
             if (err) console.log(err);
             else console.log(" " + i + " German word for: " + 
